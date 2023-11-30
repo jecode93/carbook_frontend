9d4398b996ad4aa31849b01a113fa3c152c1d624
jecode93 <jeanemmanuelcadet@gmail.com>
2023-11-30 12:45:00 -0500

    Create login and signup component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Layout from './components/Layout';
+import Login from './components/Login';
+import Signup from './components/Signup';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route
-          path="/home"
-          element={<Layout />}
-        />
+        <Route index element={<Login />} />
+        <Route path="signup" element={<Signup />} />
+        <Route path="home" element={<Layout />} />
       </Routes>
     </BrowserRouter>
   );
