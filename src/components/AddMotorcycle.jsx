93e232294007efc041a571c6a8ea41e3213a857e
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-01 10:37:40 -0500

    Fix linter errors.

--- src/components/AddMotorcycle.jsx
+++ src/components/AddMotorcycle.jsx
@@ -1,9 +1,7 @@
-import React from 'react'
+import React from 'react';
 
-const AddMotorcycle = () => {
-  return (
-    <div>AddMotorcycle</div>
-  )
-}
+const AddMotorcycle = () => (
+  <div>AddMotorcycle</div>
+);
 
-export default AddMotorcycle
+export default AddMotorcycle;
