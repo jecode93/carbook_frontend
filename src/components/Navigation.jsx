58838d0f8140570a65b58ce45d64e644daed8198
jecode93 <jeanemmanuelcadet@gmail.com>
2023-12-03 15:08:16 -0500

    Fix navigation error for desktop version by adding a desktop nav

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -12,7 +12,7 @@ const Navigation = () => {
 
   return (
     <>
-      <div className={!isOpen ? '' : 'absolute left-0 right-0 md:static h-screen md:w-3/12 bg-white duration-300'}>
+      <div className={!isOpen ? '' : 'md:hidden absolute left-0 right-0 md:static h-screen md:w-3/12 bg-white duration-300'}>
         <div // eslint-disable-line jsx-a11y/no-static-element-interactions
           className={
             isOpen
@@ -36,6 +36,44 @@ const Navigation = () => {
           <AiOutlineClose />
         </div>
         <nav className={!isOpen ? 'hidden duration-300' : 'pt-10 pl-5 flex flex-col'}>
+          <div className="pt-5">
+            <Link to="/">
+              <h1 className="font-logo font-bold text-2xl">Motor Book</h1>
+            </Link>
+          </div>
+          <ul className="pt-40 font-bold">
+            <li className="mb-4 p-2 bg-green-600 text-white">
+              <Link to="/" onClick={toggleMenu}>
+                Motorcycles
+              </Link>
+            </li>
+            <li className="mb-4 p-2">
+              <Link to="/new-motor" onClick={toggleMenu}>
+                Add motorcycle
+              </Link>
+            </li>
+            <li className="mb-4 p-2">
+              <Link to="/reservations" onClick={toggleMenu}>
+                My Reservations
+              </Link>
+            </li>
+            <li className="mb-4 p-2">
+              <Link to="/delete-motor" onClick={toggleMenu}>
+                Delete motorcycle
+              </Link>
+            </li>
+          </ul>
+          <div className="absolute bottom-10">
+            <Link to="signup" onClick={toggleMenu}>
+              Sign out
+            </Link>
+          </div>
+        </nav>
+      </div>
+
+      {/* Desktop navigation */}
+      <div className="hidden md:flex md:flex-col w-3/12 lg:w-2/12 md:h-screen pt-10 pl-5 duration-300">
+        <nav>
           <div className="pt-5">
             <Link to="/">
               <h1 className="font-logo font-bold text-2xl">Motor Book</h1>
