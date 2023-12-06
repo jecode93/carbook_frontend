67f772fba7358706fea9a513a1b4248377734c39
anshuman7negi <anshumannegi777@gmail.com>
2023-12-06 18:30:11 +0530

    fixed linter errors

--- src/components/ReservationList.jsx
+++ src/components/ReservationList.jsx
@@ -5,15 +5,25 @@ import { displayReservation } from '../redux/reservation/reservationSlice';
 const ReservationList = () => {
   const dispatch = useDispatch();
   const { display } = useSelector((store) => store.reservation);
-  console.log(display);
+  const { message } = useSelector((store) => store.bikes);
 
   useEffect(() => {
     dispatch(displayReservation());
   }, [dispatch]);
 
   return (
-    <div>
-      <h1>This is reservation List</h1>
+    <div className="flex flex-col gap-6 p-4 items-center w-full">
+      <h1 className="font-bold text-2xl mb-8">My Reservatons</h1>
+      {display.message.map((reserve) => {
+        const bike = message.bikes && message.bikes.find((bike) => bike.id === reserve.bike_id);
+        return (
+          <div className="flex gap-3 border w-2/4 p-4 items-center justify-between shadow-xl rounded-xl" key={reserve.id}>
+            <h2 className="font-bold">{bike.name}</h2>
+            <p>{reserve.city}</p>
+            <p>{reserve.date}</p>
+          </div>
+        );
+      })}
     </div>
   );
 };
