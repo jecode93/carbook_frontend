db9fe01bdfb8af108c13f99d52b2ea4336a16809
noelfoka <nomgnenoel@gmail.com>
2023-12-07 06:28:46 +0100

    fix linters error

--- src/components/DeleteMotorcycle.jsx
+++ src/components/DeleteMotorcycle.jsx
@@ -32,15 +32,13 @@ const DeleteMotorcycle = () => {
       <div className="flex flex-col gap-6 p-4 m-4 items-center w-full">
         <h1 className="font-bold text-2xl mb-8">My Reservations</h1>
         <div className="flex flex-row p-5">
-          {message.bikes.map((bike) => {
-            return (
-              <div className="flex flex-col bg-[#F9F9F9] w-[300px] gap-1 border-solid border-2 p-1 m-4 rounded-lg border-[#E1E1E1]" key={bike.id}>
-                <img src={bike.image} alt="" className="min-h-[300px] rounded-lg" />
-                <p className="text-center text-xl font-bold">{bike.name}</p>
-                <button className="primary py-2 px-4 rounded" onClick={() => handleDelete(bike.id)}>Delete</button>
-              </div>
-            );
-          })}
+          {message.bikes.map((bike) => (
+            <div className="flex flex-col bg-[#F9F9F9] w-[300px] gap-1 border-solid border-2 p-1 m-4 rounded-lg border-[#E1E1E1]" key={bike.id}>
+              <img src={bike.image} alt="" className="min-h-[300px] rounded-lg" />
+              <p className="text-center text-xl font-bold">{bike.name}</p>
+              <button type="submit" className="primary py-2 px-4 rounded" onClick={() => handleDelete(bike.id)}>Delete</button>
+            </div>
+          ))}
         </div>
       </div>
     );
