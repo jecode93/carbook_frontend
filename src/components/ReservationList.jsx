e31a4faf06264213cf87a4fbf706d86b04731352
anshuman7negi <anshumannegi777@gmail.com>
2023-12-07 20:49:21 +0530

    fixed linters

--- src/components/ReservationList.jsx
+++ src/components/ReservationList.jsx
@@ -1,6 +1,7 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { displayReservation } from '../redux/reservation/reservationSlice';
+import { getBikes } from '../redux/Bikes/bikeSlice';
 
 const ReservationList = () => {
   const dispatch = useDispatch();
@@ -8,6 +9,7 @@ const ReservationList = () => {
   const { message } = useSelector((store) => store.bikes);
 
   useEffect(() => {
+    dispatch(getBikes());
     dispatch(displayReservation());
   }, [dispatch]);
 
@@ -25,7 +27,7 @@ const ReservationList = () => {
           const bike = message.bikes && message.bikes.find((bike) => bike.id === reserve.bike_id);
           return (
             <div className="flex gap-3 border w-full  md:w-3/4 p-4 items-center justify-between shadow-xl rounded-xl" key={reserve.id}>
-              <h2 className="font-bold">{bike.name}</h2>
+              <h2 className="font-bold">{bike && bike.name}</h2>
               <p>{reserve.city}</p>
               <p>{reserve.date}</p>
             </div>
