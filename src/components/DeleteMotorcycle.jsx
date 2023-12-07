905777b5e924fc6b3fd22359dfd309d6de1d8792
noelfoka <nomgnenoel@gmail.com>
2023-12-07 05:10:59 +0100

    add style

--- src/components/DeleteMotorcycle.jsx
+++ src/components/DeleteMotorcycle.jsx
@@ -29,17 +29,19 @@ const DeleteMotorcycle = () => {
 
   if (message) {
     return (
-      <div className="flex flex-col gap-6 p-4 items-center w-full">
+      <div className="flex flex-col gap-6 p-4 m-4 items-center w-full">
         <h1 className="font-bold text-2xl mb-8">My Reservations</h1>
-        {message.bikes.map((bike) => {
-          return (
-            <div key={bike.id}>
-              <h2>{bike.name}</h2>
-              <p>{bike.total_amount_payable}</p>
-              <button onClick={() => handleDelete(bike.id)}>Delete</button>
-            </div>
-          );
-        })}
+        <div className="flex flex-row p-5">
+          {message.bikes.map((bike) => {
+            return (
+              <div className="flex flex-col bg-[#F9F9F9] w-[300px] gap-1 border-solid border-2 p-1 m-4 rounded-lg border-[#E1E1E1]" key={bike.id}>
+                <img src={bike.image} alt="" className="min-h-[300px] rounded-lg" />
+                <p className="text-center text-xl font-bold">{bike.name}</p>
+                <button className="primary py-2 px-4 rounded" onClick={() => handleDelete(bike.id)}>Delete</button>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
