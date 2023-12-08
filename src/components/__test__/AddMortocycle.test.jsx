a131fefaa95db1b867ff2ed7dfa4a368e3cca098
okari <nyandika15@gmail.com>
2023-12-08 17:10:56 +0300

    Add test cases

--- src/components/__test__/AddMortocycle.test.jsx
+++ src/components/__test__/AddMortocycle.test.jsx
@@ -38,35 +38,4 @@ describe('AddMotorcycle', () => {
       </Provider>,
     );
   });
-
-  test('View when there is data in the mock Storage', () => {
-    store = mockStore({
-      bikes: {
-        message: [
-          {
-            name: 'Test bike 001',
-            image: 'https://i.ebayimg.com/images/g/nPcAAOSwUCRkO9sD/s-l500.jpg',
-            description: 'Test bike 00 ',
-            deposit: '2000',
-            finance_fee: '1000',
-            option_to_purchase_fee: '1000',
-            total_amount_payable: '1000',
-            duration: '1000',
-          },
-        ],
-        isLoading: false,
-        error: null,
-      },
-    });
-  
-    const tree = renderer
-      .create(
-        <Provider store={store}>
-          <AddMotorcycle />
-        </Provider>,
-      )
-      .toJSON();
-    expect(tree).toMatchSnapshot();
-    expect(tree).toContainHTML('<label class="font-bold">Bike Name</label>');
-  });  
 });
