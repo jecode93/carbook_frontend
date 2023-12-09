3bf16e712c6c3654ecd763929c3f820e001d76d6
anshuman7negi <anshumannegi777@gmail.com>
2023-12-09 09:40:26 +0530

    Added snapshot test for reservation

---
+++ src/components/__test__/Reservation.test.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { Provider } from 'react-redux';
+import configureStore from 'redux-mock-store';
+import ReservationList from '../ReservationList';
+
+const mockStore = configureStore([]);
+
+describe('ReservationList Component', () => {
+  let store;
+  let component;
+
+  beforeEach(() => {
+    store = mockStore({
+      reservation: {
+        display: {
+          message: [
+            { id: 1, bike_id: 1, city: 'City1', date: '2023-12-31' },
+            { id: 2, bike_id: 2, city: 'City2', date: '2023-12-30' },
+          ],
+        },
+        isLoading: false,
+      },
+      bikes: {
+        message: {
+          bikes: [
+            { id: 1, name: 'Bike1' },
+            { id: 2, name: 'Bike2' },
+          ],
+        },
+      },
+    });
+
+    component = renderer.create(
+      <Provider store={store}>
+        <ReservationList />
+      </Provider>
+    );
+  });
+
+  it('renders correctly', () => {
+    expect(component.toJSON()).toMatchSnapshot();
+  });
+});
