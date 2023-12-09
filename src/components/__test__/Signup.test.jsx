21b137f114efa898beb794f9c6d5e3cb3c4a99a1
anshuman7negi <anshumannegi777@gmail.com>
2023-12-09 09:18:18 +0530

    Added signup test

--- src/components/__test__/Signup.test.jsx
+++ src/components/__test__/Signup.test.jsx
@@ -2,13 +2,22 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
 import Signup from '../Signup';
+import { signup } from '../../redux/auth/authSlice';
 
+const mockStore = configureStore([]);
 
 describe('Signup Component', () => {
+  let store;
   let component;
 
   beforeEach(() => {
+    store = mockStore({
+      message: '',
+    });
+
+    store.dispatch = jest.fn();
 
     component = renderer.create(
       <Provider store={store}>
@@ -22,5 +31,5 @@ describe('Signup Component', () => {
   it('renders correctly', () => {
     expect(component.toJSON()).toMatchSnapshot();
   });
-
+  
 });
